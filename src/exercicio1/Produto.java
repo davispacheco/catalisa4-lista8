f342025ef2d0a29f846975ada56022a9efb7b40e
davispacheco <davipacheco1996@gmail.com>
2022-07-15 16:43:07 -0300

    Criar classe Produto.

---
+++ src/exercicio1/Produto.java
@@ -0,0 +1,27 @@
+package src.exercicio1;
+
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto() {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
